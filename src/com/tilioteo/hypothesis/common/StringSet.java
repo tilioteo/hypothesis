b13a1efd925f290e8dc34d110db3c589566ba75f
Kamil Morong <kamil.morong@gmail.com>
2014-01-23 19:45:09 +0100

    updated StringSet allows initialization from other StringSet and additional string array

--- src/com/tilioteo/hypothesis/common/StringSet.java
+++ src/com/tilioteo/hypothesis/common/StringSet.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 
 /**
  * @author Kamil Morong - Hypothesis
- *
+ * 
  */
 @SuppressWarnings("serial")
 public class StringSet extends HashSet<String> {
@@ -15,12 +15,24 @@ public class StringSet extends HashSet<String> {
 	public StringSet() {
 		super();
 	}
-	
-	public StringSet(String[] strings) {
+
+	public StringSet(StringSet stringSet, String[] strings) {
 		this();
-		
+
+		if (stringSet != null)
+			addAll(stringSet);
+
 		if (strings != null)
 			for (String str : strings)
 				add(str);
 	}
+
+	public StringSet(String[] strings) {
+		this(null, strings);
+	}
+
+	public StringSet(StringSet stringSet) {
+		this(stringSet, null);
+	}
+
 }
