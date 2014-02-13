dd5a72b7ba85bcb345d9399117a711f51081187b
Kamil Morong <kamil.morong@gmail.com>
2014-02-13 15:52:31 +0100

    added SpringHibernateVaadinServlet

---
+++ src/com/tilioteo/hypothesis/servlet/SpringHibernateVaadinServlet.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.tilioteo.hypothesis.servlet;
+
+/**
+ * @author kamil
+ *
+ */
+@SuppressWarnings("serial")
+public class SpringHibernateVaadinServlet extends HibernateVaadinServlet {
+
+}
