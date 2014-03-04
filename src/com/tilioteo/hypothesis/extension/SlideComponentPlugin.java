81d9ddc7e4873ce6eb7c57c560bfb9c5f0b86bfe
Kamil Morong <kamil.morong@gmail.com>
2014-03-04 15:25:44 +0100

    extension plugin support

--- src/com/tilioteo/hypothesis/extension/SlideComponentPlugin.java
+++ src/com/tilioteo/hypothesis/extension/SlideComponentPlugin.java
@@ -3,13 +3,21 @@
  */
 package com.tilioteo.hypothesis.extension;
 
+import java.util.Map;
 import java.util.Set;
+
+import org.dom4j.Element;
+
+import com.tilioteo.hypothesis.core.SlideManager;
+import com.tilioteo.hypothesis.ui.SlideComponent;
 
 /**
  * @author kamil
  *
  */
 public interface SlideComponentPlugin extends Plugin {
+	
+	public enum ValidParentGroup { VIEWPORT, PANEL, CONTAINER };
 
 	/**
 	 * XML namespace of plugin elements
@@ -26,5 +34,9 @@ public interface SlideComponentPlugin extends Plugin {
 	 */
 	public Set<String> getElements();
 	
+	public Set<String> getEventTypes();
 	
+	public Map<String, Set<ValidParentGroup>> getElementParentGroups();
+	
+	public SlideComponent createComponentFromElement(Element element, SlideManager slideManager);
 }
