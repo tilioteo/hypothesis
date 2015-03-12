fdf72d28158e95e45f8af77890773c58cb928565
Kamil Morong <kamil.morong@gmail.com>
2015-03-12 12:15:03 +0100

    moved websocket component to its own project

--- src/org/vaadin/websocket/shared/ui/websocket/WebSocketClientRpc.java
+++
@@ -1,9 +0,0 @@
-package org.vaadin.websocket.shared.ui.websocket;
-
-import com.vaadin.shared.communication.ClientRpc;
-
-public interface WebSocketClientRpc extends ClientRpc {
-
-	public void send(String message);
-
-}
