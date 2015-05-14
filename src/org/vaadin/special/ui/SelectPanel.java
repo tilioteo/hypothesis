a7998764bfbba7533587096ecc4b0e595c6ffe43
Kamil Morong <kamil.morong@gmail.com>
2015-05-14 13:56:53 +0200

    code formating

--- src/org/vaadin/special/ui/SelectPanel.java
+++ src/org/vaadin/special/ui/SelectPanel.java
@@ -66,7 +66,7 @@ public class SelectPanel extends MultipleComponentPanel<SelectButton> implements
             return Collections.unmodifiableCollection(selectedButtons);
         }
 	}
-
+	
 	public void setLabelPosition(LabelPosition labelPosition) {
 		if (this.labelPosition != labelPosition) {
 			this.labelPosition = labelPosition;
