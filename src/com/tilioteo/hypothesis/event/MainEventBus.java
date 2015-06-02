33b6949150f223b5fa8842875651be652182ec8d
Kamil Morong <kamil.morong@gmail.com>
2015-06-02 14:29:34 +0200

    fixed bus behaviour, bus mapped to ui instance instead of singleton which caused event propagation to other ui's handlers

--- src/com/tilioteo/hypothesis/event/MainEventBus.java
+++ src/com/tilioteo/hypothesis/event/MainEventBus.java
@@ -3,7 +3,11 @@
  */
 package com.tilioteo.hypothesis.event;
 
+import java.util.HashMap;
+
 import com.tilioteo.hypothesis.event.HypothesisEvent.MainUIEvent;
+import com.tilioteo.hypothesis.ui.MainUI;
+import com.vaadin.ui.UI;
 
 /**
  * @author kamil
@@ -12,9 +16,22 @@ import com.tilioteo.hypothesis.event.HypothesisEvent.MainUIEvent;
 @SuppressWarnings("serial")
 public class MainEventBus extends HypothesisEventBus<MainUIEvent> {
 	
-	private static final MainEventBus instance = new MainEventBus();
+	private static HashMap<MainUI, MainEventBus> map = new HashMap<MainUI, MainEventBus>();
+	
+	public static final void createInstance(MainUI ui) {
+		map.put(ui, new MainEventBus());
+	}
 	
 	public static final MainEventBus get() {
-		return instance;
+		UI ui = UI.getCurrent();
+		if (ui instanceof MainUI) {
+			return map.get((MainUI)ui);
+		}
+		
+		return null;
 	}
+	
+	protected MainEventBus() {
+	}
+
 }
--- src/com/tilioteo/hypothesis/event/ProcessEventBus.java
+++ src/com/tilioteo/hypothesis/event/ProcessEventBus.java
@@ -3,7 +3,11 @@
  */
 package com.tilioteo.hypothesis.event;
 
+import java.util.HashMap;
+
 import com.tilioteo.hypothesis.event.HypothesisEvent.ProcessUIEvent;
+import com.tilioteo.hypothesis.ui.ProcessUI;
+import com.vaadin.ui.UI;
 
 /**
  * @author kamil
@@ -12,9 +16,22 @@ import com.tilioteo.hypothesis.event.HypothesisEvent.ProcessUIEvent;
 @SuppressWarnings("serial")
 public class ProcessEventBus extends HypothesisEventBus<ProcessUIEvent> {
 
-	private static final ProcessEventBus instance = new ProcessEventBus();
+	private static HashMap<ProcessUI, ProcessEventBus> map = new HashMap<ProcessUI, ProcessEventBus>();
+	
+	public static final void createInstance(ProcessUI ui) {
+		map.put(ui, new ProcessEventBus());
+	}
 	
 	public static final ProcessEventBus get() {
-		return instance;
+		UI ui = UI.getCurrent();
+		if (ui instanceof ProcessUI) {
+			return map.get((ProcessUI)ui);
+		}
+		
+		return null;
 	}
+	
+	protected ProcessEventBus() {
+	}
+
 }
