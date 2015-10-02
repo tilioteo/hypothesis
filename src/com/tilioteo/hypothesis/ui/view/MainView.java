0065d1b66f437c2c780d1ea22667df11f4457583
Kamil Morong <kamil.morong@gmail.com>
2015-10-02 17:33:08 +0200

    updated version info

--- src/com/tilioteo/hypothesis/ui/view/MainView.java
+++ src/com/tilioteo/hypothesis/ui/view/MainView.java
@@ -14,7 +14,7 @@ import com.vaadin.ui.themes.ValoTheme;
 @SuppressWarnings("serial")
 public class MainView extends VerticalLayout {
 	
-	private static final String VERSION = "1.4.6";
+	private static final String VERSION = "1.4.9";
 
 	public MainView() {
 		setSizeFull();
